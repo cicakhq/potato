22512de885bbd9e88f91985a76646de22fa98cf8
Mathieu Legrand <mathieu@legrand.im>
2016-01-01 15:24:52 +0800

    Use the shared file instead of copying it to the virtual machine.

--- deploy/provision.sh
+++ deploy/provision.sh
@@ -3,15 +3,25 @@
 SBCL_PATH=""
 OS=$(uname -s)
 
+POTATO_ROOT_FOLDER="$1"
+POTATO_LISP_SCRIPT="$2"
+POTATO_FULL_SCRIPT="$POTATO_ROOT_FOLDER/$POTATO_LISP_SCRIPT"
+
 if [ "$OS" == "FreeBSD" ]; then
-    /usr/sbin/pkg install -y sbcl
+    echo "Installing SBCL"
+    /usr/sbin/pkg install -q -y sbcl
     SBCL_PATH="/usr/local/bin/sbcl"
 fi
 
-if [ -n "$SBCL_PATH" ] && [ -x "$SBCL_PATH" ]; then
-    exec $SBCL_PATH --script "/var/tmp/provision.lisp"
+if [ -f "$POTATO_FULL_SCRIPT" ]; then
+    if [ -n "$SBCL_PATH" ] && [ -x "$SBCL_PATH" ]; then
+        exec $SBCL_PATH --script "$POTATO_FULL_SCRIPT"
+    else
+        >&2 echo "$SBCL_PATH is not executable ($OS)."
+        exit 1
+    fi
 else
-    >&2 echo "$SBCL_PATH is not executable ($OS)"
+    >&2 echo "$POTATO_FULL_SCRIPT wasn't found."
     exit 1
 fi
 
