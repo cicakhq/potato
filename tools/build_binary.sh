b16868f93753fb0afb3cd9df6d3099e628883b5a
Elias Martenson <lokedhs@gmail.com>
2016-02-10 13:49:20 +0800

    Added some comments

--- tools/build_binary.sh
+++ tools/build_binary.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
+#
+#  The dumped binary calls SERVER-CONFIG2-LAUNCH-SERVICE which
+#  processes the configuration file, startup options and allows the
+#  user to choose which service to start.
+#
+
 sbcl --non-interactive --disable-debugger --load server-config2.lisp --eval '(progn (sb-ext:disable-debugger) (sb-ext:save-lisp-and-die "potato.bin" :toplevel #'"'"'server-config2:launch-service :executable t))'
