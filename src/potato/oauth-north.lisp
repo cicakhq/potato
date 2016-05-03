7d61828d87a8d357b43584faee321e2401def26e
Elias Martenson <lokedhs@gmail.com>
2016-05-03 16:22:06 +0800

    oauth-north code now compiles

--- src/potato/oauth-north.lisp
+++ src/potato/oauth-north.lisp
@@ -2,7 +2,10 @@
 
 (declaim #.potato.common::*compile-decl*)
 
-(defvar *server* (make-instance 'north:simple-server))
+(defclass potato-north-server (north:server)
+  ())
+
+(defvar *server* (make-instance 'potato-north-server))
 
 (defun make-request (request)
   (make-instance 'north:request
@@ -17,9 +20,9 @@
   (multiple-value-bind (token secret callback-confirmed)
       (north:oauth/request-token *server* (make-request hunchentoot:*request*))
     (setf (hunchentoot:content-type*) "text/plain")
-    (north:alist->oauth-response `(("oauth_token" ,token)
-                                   ("oauth_token_secret" ,secret)
-                                   ("oauth_callback_confirmed" ,(if callback-confirmed :true :false))))))
+    (north:alist->oauth-response `(("oauth_token" . ,token)
+                                   ("oauth_token_secret" . ,secret)
+                                   ("oauth_callback_confirmed" . ,(if callback-confirmed :true :false))))))
 
 (lofn:define-handler-fn (oauth-authorise "/oauth/authorize" nil ())
   (lofn:with-checked-parameters ((oauth-token :name "oauth_token" :required t :allow-blank nil)
@@ -36,7 +39,24 @@
                                    (:error . ,error-link))))))
 
 (lofn:define-handler-fn (oauth-authenticate "/oauth/authenticate" nil ())
-  )
+  (lofn:with-checked-parameters (action)
+    (string-case:string-case (action)
+      ("deny" "Not allowed")
+      ("allow" (multiple-value-bind (token verifier url)
+                   (north:oauth/authorize *server* (make-request hunchentoot:*request*))
+                 (if url
+                     (hunchentoot:redirect url)
+                     (hunchentoot:redirect (format nil "/oauth/authorize?oauth_token=~a&verifier=~a"
+                                                   (north:url-encode token) (north:url-encode verifier))))))
+      (t
+       (hunchentoot:redirect "/oauth/authorize?error=Invalid_action")))))
 
 (lofn:define-handler-fn (oauth-access-token "/oauth/access-token" nil ())
-  )
+  (multiple-value-bind (token secret)
+      (north:oauth/access-token *server* (make-request hunchentoot:*request*))
+    (north:alist->oauth-response `(("oauth_token" . ,token)
+                                   ("oauth_token_secret" . ,secret)))))
+
+(lofn:define-handler-fn (oauth-verify "/oauth/verify" nil ())
+  (north:oauth/verify *server* (make-request hunchentoot:*request*))
+  (north:alist->oauth-response `(("status" . "success"))))
