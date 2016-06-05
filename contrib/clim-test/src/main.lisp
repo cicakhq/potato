9da5fc7e8f1eb0f2c1006be874e612d722d75567
Elias Martenson <lokedhs@gmail.com>
2016-06-06 04:29:39 +0800

    Some more work on the clim client

--- contrib/clim-test/src/main.lisp
+++ contrib/clim-test/src/main.lisp
@@ -1,17 +1,30 @@
 (defpackage :clim-test
   (:use :cl))
 
-(in-package :clim-test)
-
 (clim:define-application-frame potato-frame ()
-  ()
-  (:panes (channel-list :application)
+  ((connection :type connection-state
+               :initarg :connection
+               :reader potato-frame/connection))
+  (:panes (channel-list :application
+                        :display-function 'display-channel-list)
           (channel-content :application))
   (:layouts (default (clim:horizontally ()
                        (2/10 channel-list)
                        (8/10 channel-content)))))
 
-(defun potato-client-clim ()
-  (let ((frame (clim:make-application-frame 'potato-frame :width 500 :height 700)))
-    (defparameter *frame* frame)
+(defun display-channel-list (frame stream)
+  (let ((conn (potato-frame/connection frame)))
+    (clim:formatting-table (stream :x-spacing 5 :y-spacing 5)
+      (loop
+        for channel in (connection-state/channels conn)
+        do (clim:formatting-row (stream)
+             (clim:formatting-cell (stream)
+               (clim:draw-text* stream (channel/name channel) 10 10)))))))
+
+(defparameter *frame* nil)
+
+(defun potato-client-clim (api-key)
+  (let* ((connection (make-instance 'connection-state :api-key api-key))
+         (frame (clim:make-application-frame 'potato-frame :connection connection :width 500 :height 700)))
+    (setq *frame* frame)
     (clim:run-frame-top-level frame)))
