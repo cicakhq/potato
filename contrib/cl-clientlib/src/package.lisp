47ee93a804acd79980873c2cce3799d8204be117
Elias Martenson <lokedhs@gmail.com>
2016-08-18 18:25:53 +0800

    Added some more exported symbols

--- contrib/cl-clientlib/src/package.lisp
+++ contrib/cl-clientlib/src/package.lisp
@@ -1,8 +1,14 @@
 (defpackage :potato-client
   (:use :cl)
-  (:export
-   #:*connection*
-   #:list-domains
-   #:connection
-   #:connection/api-key
-   #:connection/url-prefix))
+  (:export #:*connection*
+           #:list-domains
+           #:connection
+           #:connection/api-key
+           #:connection/url-prefix
+           #:send-message
+           #:load-channel-tree
+           #:load-channel
+           #:request-error/code
+           #:request-error/reason
+           #:request-error
+           #:potato-client-error))
