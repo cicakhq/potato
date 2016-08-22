8de004f7e58165d472d128469bc2dcb9845ee52a
Elias Martenson <lokedhs@gmail.com>
2016-08-22 11:08:29 +0800

    Added some notes regarding the CLIM command problem (possibly a McCLIM bug)

--- contrib/potato-client-clim/src/main.lisp
+++ contrib/potato-client-clim/src/main.lisp
@@ -13,23 +13,39 @@
   (:panes (channel-list :application
                         :display-function 'display-channel-list)
           (channel-content :application)
-          (int :interactor))
-  (:layouts (default (clim:vertically ()
-                       (clim:horizontally ()
-                         (2/10 channel-list)
-                         (8/10 channel-content)))
-                     int)))
+          (interaction-pane :interactor))
+  (:layouts (default (9/10 (clim:vertically ()
+                             (clim:horizontally ()
+                               (2/10 channel-list)
+                               (8/10 channel-content))))
+                     (1/10 interaction-pane))))
 
 (clim:define-presentation-method clim:present (obj (type channel) stream view &key)
-  (log:info "Calling present method for channel: ~s" obj)
-  (clim:draw-text* stream (channel/name obj) 10 10))
+  (log:info "Calling present method for channel: ~s, stream: ~s" obj stream)
+  ;;
+  ;; PROBLEM 1:
+  ;;
+  ;; The below command fails when CLIM is trying to format the command
+  ;; for display in the command panel. This is because DRAW-TEXT*
+  ;; can't be used on the string stream that is used in
+  ;; DREI::PRESENT-ACCEPTABLY-TO-STRING
+  ;;
+  #+nil(clim:draw-text* stream (channel/name obj) 10 10)
+  ;;
+  ;; PROBLEM 2:
+  ;;
+  ;; To work around problem 1, I'm using PRINC instead of DRAW-TEXT*.
+  ;; When I do that, we see that the OBJ parameter is NIL here. That
+  ;; should really never happen.
+  ;;
+  (princ (if obj (channel/name obj) "<NULL>") stream))
 
 (clim:define-presentation-to-command-translator select-channel
     (channel switch-to-channel-frame potato-frame)
     (obj)
   (list obj))
 
-(define-potato-frame-command (switch-to-channel-frame :name "Switch to channel frame")
+(define-potato-frame-command (switch-to-channel-frame :name "Switch to channel")
     ((obj 'channel))
   (log:info "Switch to channel command called. Type: ~s" (type-of obj)))
 
@@ -44,6 +60,6 @@
 (defparameter *frame* nil)
 
 (defun potato-client-clim ()
-  (let* ((frame (clim:make-application-frame 'potato-frame :width 500 :height 700)))
+  (let* ((frame (clim:make-application-frame 'potato-frame :width 700 :height 500)))
     (setq *frame* frame)
     (clim:run-frame-top-level frame)))
