e828057551e59dcede9c8961c9106fe2c2299250
Mathieu Legrand <mathieu.legrand@murex.com>
2017-08-04 01:07:47 +0800

    Port make-css.sh to macOS: readlink now is realpath; and md5sum is md5 -r

--- web-app/make-css.sh
+++ web-app/make-css.sh
@@ -7,8 +7,14 @@ echo "Compile the CSS files"
 SASSC=sassc
 SCSS_SRC=src/css
 CSS_OUT=../public/assets/css
-MANIFEST_DIR=$(readlink -e ../src/template/)
+MANIFEST_DIR=$(realpath -e ../src/template/)
 MANIFEST="$MANIFEST_DIR/manifest"
+set +e
+MD5SUM=`which md5sum`
+set -e
+if [ ! -x "$MD5SUM" ]; then
+  MD5SUM='md5 -r'
+fi
 
 if [ ! -d $CSS_OUT ] && [ -d ../public ]; then
 	 mkdir -p $CSS_OUT
@@ -20,7 +26,7 @@ function make_scss {
   do
 	  BASE=${f%.*}
 	  ${SASSC} $f $2/$BASE.css
-	  CKSUM_NAME=$BASE-`md5sum $2/$BASE.css | cut -b 26-32`.css
+	  CKSUM_NAME=$BASE-`$MD5SUM $2/$BASE.css | cut -b 26-32`.css
 	  mv $2/$BASE.css $2/$CKSUM_NAME
 	  echo '  ("'$BASE.css'" . "'$CKSUM_NAME'")' >> $MANIFEST
   done
@@ -33,5 +39,5 @@ function clean {
 
 clean $CSS_OUT
 echo "(" > $MANIFEST
-make_scss $(readlink -e $SCSS_SRC) $(readlink -e $CSS_OUT)
+make_scss $(realpath -e $SCSS_SRC) $(realpath -e $CSS_OUT)
 echo ")" >> $MANIFEST
