5ec38c194daafbb662f63ab88a4a992f97fde8d4
Mathieu Legrand <mathieu.legrand@murex.com>
2017-08-04 20:37:40 +0800

    Formatting

--- web-app/project.clj
+++ web-app/project.clj
@@ -1,5 +1,5 @@
-(defproject potato "0.2.0-SNAPSHOT"
-  :description        "A Potato front-end implemented"
+(defproject potato    "0.2.0-SNAPSHOT"
+  :description        "A Potato web-frontend"
   :url                "http://potato.network"
   :license            {:name "Apache"}
   :min-lein-version   "2.7.1"
@@ -7,7 +7,7 @@
   :dependencies [[org.clojure/clojure       "1.8.0"   ]
                  [org.clojure/clojurescript "1.9.854" :scope "provided"]
                  [org.clojure/core.async    "0.3.443" ]
-                 [cljsjs/preact             "7.1.0-0"]
+                 [cljsjs/preact             "7.1.0-0" ]
                  [http-kit                  "2.1.19"  ]
                  [cljs-http                 "0.1.43" :exclusions [org.clojure/core.async]]
                  [cljsjs/moment             "2.9.0-0" ]]
@@ -17,10 +17,10 @@
 
   :clean-targets ^{:protect false} ["resources/public/js"]
 
-  :profiles {:dev {:cljsbuild
-                   {:builds {:client {:figwheel   {:websocket_url "ws://localhost:3450/figwheel-ws"
+  :profiles {:dev  {:cljsbuild
+                    {:builds {:client {:figwheel  {:websocket_url "ws://localhost:3450/figwheel-ws"
                                                    :on-jsload     "potato.core/main"}
-                                      :compiler   {:main          "potato.core"
+                                       :compiler  {:main          "potato.core"
                                                    :asset_path    "/js/out"
                                                    :optimizations :none
                                                    :source_map    true
