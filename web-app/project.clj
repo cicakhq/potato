18cd824563278549578bb25b3a924a8e98b2e620
Elias Martenson <lokedhs@gmail.com>
2017-12-19 12:23:54 +0800

    Updated cide nrepl to 0.16.0

--- web-app/project.clj
+++ web-app/project.clj
@@ -26,7 +26,7 @@
                                                                    ring/ring-core
                                                                    commons-fileupload
                                                                    clj-time]]
-                             [cider/cider-nrepl "0.15.0-SNAPSHOT" :exclusions [org.clojure/clojure
+                             [cider/cider-nrepl "0.16.0-SNAPSHOT" :exclusions [org.clojure/clojure
                                                                                org.clojure/tools.nrepl]]]}}
 
   :cljsbuild {:builds
