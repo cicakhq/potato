afe648205a884fa0e4a19c4cb9cd874385c921c7
Mathieu Legrand <mathieu@legrand.im>
2018-03-22 14:34:06 +0800

    fix for macOS

--- web-app/make-css.sh
+++ web-app/make-css.sh
@@ -4,10 +4,17 @@ set -e
 
 echo "Compile the CSS files"
 
+READLINK=readlink
+MD5SUM=md5sum
+if [ "`uname -s`" == 'Darwin' ]; then
+	echo 'macOS - readlink and md5sum from coreutils'
+	READLINK=greadlink
+	MD5SUM=gmd5sum
+fi
 SASSC=sassc
 SCSS_SRC=src/css
 CSS_OUT=../public/assets/css
-MANIFEST_DIR=$(readlink -e ../src/template/)
+MANIFEST_DIR=$($READLINK -e ../src/template/)
 MANIFEST="$MANIFEST_DIR/manifest"
 
 if [ ! -d $CSS_OUT ] && [ -d ../public ]; then
@@ -20,7 +27,7 @@ make_scss () {
   do
 	  BASE=${f%.*}
 	  ${SASSC} $f $2/$BASE.css
-	  CKSUM_NAME=$BASE-`md5sum $2/$BASE.css | cut -b 26-32`.css
+	  CKSUM_NAME=$BASE-`$MD5SUM $2/$BASE.css | cut -b 26-32`.css
 	  mv $2/$BASE.css $2/$CKSUM_NAME
 	  echo '  ("'$BASE.css'" . "'$CKSUM_NAME'")' >> $MANIFEST
   done
@@ -33,5 +40,5 @@ clean () {
 
 clean $CSS_OUT
 echo "(" > $MANIFEST
-make_scss $(readlink -e $SCSS_SRC) $(readlink -e $CSS_OUT)
+make_scss $($READLINK -e $SCSS_SRC) $($READLINK -e $CSS_OUT)
 echo ")" >> $MANIFEST
