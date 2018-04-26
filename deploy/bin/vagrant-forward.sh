7060292bf0195c4d244c13819c201406a3d8909d
Mathieu Legrand <mathieu@legrand.im>
2018-04-26 11:30:46 +0800

    Remove old things

--- deploy/bin/vagrant-forward.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-exec vagrant ssh -- -N -o ServerAliveInterval=240 -L15672:localhost:15672 -L4005:localhost:4005 -L4006:localhost:4006 -L4007:localhost:4007 -L4009:localhost:4009 -L5984:localhost:5984 -L8983:localhost:8983 -L*:8080:localhost:8180 -L*:8081:localhost:8081
